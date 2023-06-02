8a9786eee1dd3edf70eee9887a1b975af81e9469
Andresa Marçal <andresamarques3107@gmail.com>
2023-06-02 18:48:22 -0300

    update

--- blogPessoal/src/App.tsx
+++ blogPessoal/src/App.tsx
@@ -1,36 +1,54 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/estaticos/navbar/Navbar';
-import Home from './paginas/home/Home';
-import Sobre from './paginas/sobre/Sobre';
-import Usuarios from './paginas/usuarios/Usuarios';
-import Galeria from './paginas/galeria/Galeria';
+import { Provider } from 'react-redux';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Footer from './components/estaticos/footer/Footer';
-import Login from './paginas/login/Login';
-import './App.css'
-import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Navbar from './components/estaticos/navbar/Navbar';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import ListaTema from './components/temas/listatema/ListaTema';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import Perfil from './paginas/perfil/Perfil';
+import store from './store/Store';
+
+import 'react-toastify/dist/ReactToastify.css';
+import AtualizarUsuario from './components/usuarios/atualizarusuario/AtualizarUsuario';
+import ListaPostagemPorTitulo from './components/postagens/listapostagemportitulo/ListaPostagemPorTitulo';
 
 function App() {
+
+
   return (
-    <>
-        <BrowserRouter>
-          <Navbar />
+    <Provider store={store}>
+      <ToastContainer />
+      <Router>
+        <Navbar />
+        <div style={{ minHeight: '100vh' }}>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/sobre" element={<Sobre />} />
-            <Route path="/post" element={<ListaPostagem />} />
-            <Route path="/temas" element={<ListaTema />} />
-            <Route path="/usuarios" element={<Usuarios />} />
-            <Route path="/galeria" element={<Galeria />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<CadastroUsuario />} />
+            <Route path="/cadastrousuario" element={<CadastroUsuario />} />
+            <Route path="/temas" element={<ListaTema />} />
+            <Route path="/posts" element={<ListaPostagem />} />
+            <Route path="/formularioPostagem" element={<CadastroPost />} />
+            <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+            <Route path="/formularioTema" element={<CadastroTema />} />
+            <Route path="/formularioTema/:id" element={<CadastroTema />} />
+            <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+            <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            <Route path="/perfil" element={<Perfil />} />
+            <Route path="/atualizarusuario" element={<AtualizarUsuario />} />
+            <Route path="/postagensportitulo" element={<ListaPostagemPorTitulo />} />
           </Routes>
-          <Footer />
-        </BrowserRouter>
-    </>
+        </div>
+        <Footer />
+      </Router>
+    </Provider>
   )
 }
 
